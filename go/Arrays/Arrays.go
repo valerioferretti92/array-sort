ceffe58c88f1a3fd3c232fbbffc1b56d22967362
Valerio Ferretti <valerio.ferretti@symphony.com>
2020-12-06 20:28:52 +0100

    Implementation of MergeSort and TrivialSort with GO

---
+++ go/Arrays/Arrays.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+const maxUint = ^uint(0)
+
+func main() {
+	algorithmName, arraySize, err := parseCmdlArgs()
+	if err != nil {
+		fmt.Println(err.Error())
+		flag.Usage()
+		return
+	}
+
+	//Array initialisation
+	myArray := make([]uint, arraySize, arraySize)
+	initialize(myArray)
+
+	//Algorithm execution
+	var algorithm func([]uint)
+	if algorithmName == "TrivialSort" {
+		algorithm = trivialSort
+	} else if algorithmName == "MergeSort" {
+		algorithm = mergeSort
+	} else {
+		fmt.Println("ERROR: invalid algorithm specified")
+		return
+	}
+	t1 := time.Now()
+	algorithm(myArray)
+	d := time.Since(t1)
+	if !checkArray(myArray) {
+		fmt.Println("ERROR: The array has not been properly sorted")
+		return
+	}
+
+	//Printing results to stdout
+	fmt.Printf("Algorithm: %s\n", algorithmName)
+	fmt.Printf("Problem Size: %d\n", arraySize)
+	fmt.Printf("Min Value: %d\n", myArray[0])
+	fmt.Printf("Max Value: %d\n", myArray[len(myArray)-1])
+	s := int(d.Seconds())
+	ms := d.Milliseconds() % 1000
+	us := d.Microseconds() % 1000
+	fmt.Printf("Execution Time: %vs %vms %vus\n", s, ms, us)
+}
+
+func trivialSort(myArray []uint) {
+	for i, temp := range myArray {
+		var indexMin int
+		var min uint = maxUint
+		for j := i; j < len(myArray); j++ {
+			if myArray[j] < min {
+				min = myArray[j]
+				indexMin = j
+			}
+		}
+		myArray[i] = min
+		myArray[indexMin] = temp
+	}
+}
+
+func mergeSort(myArray []uint) {
+	divide(myArray, 0, len(myArray)-1)
+}
+
+func divide(myArray []uint, low, high int) {
+	if low == high {
+		return
+	}
+	mid := (low + high) / 2
+	divide(myArray, low, mid)
+	divide(myArray, mid+1, high)
+	merge(myArray, low, mid, mid+1, high)
+}
+
+func merge(myArray []uint, leftLow, leftHigh, rightLow, rightHigh int) {
+	length := rightHigh - leftLow + 1
+	left := leftLow
+	right := rightLow
+	tempArray := make([]uint, length, length)
+	for i := range tempArray {
+		if left > leftHigh {
+			tempArray[i] = myArray[right]
+			right++
+		} else if right > rightHigh {
+			tempArray[i] = myArray[left]
+			left++
+		} else if myArray[left] <= myArray[right] {
+			tempArray[i] = myArray[left]
+			left++
+		} else if myArray[right] <= myArray[left] {
+			tempArray[i] = myArray[right]
+			right++
+		}
+	}
+
+	for i := range tempArray {
+		myArray[leftLow] = tempArray[i]
+		leftLow++
+	}
+}
+
+func checkArray(myArray []uint) bool {
+	for i := 1; i < len(myArray); i++ {
+		if myArray[i] < myArray[i-1] {
+			return false
+		}
+	}
+	return true
+}
+
+func initialize(myArray []uint) {
+	rand.Seed(time.Now().UnixNano())
+	for i := range myArray {
+		myArray[i] = uint(rand.Int())
+	}
+	return
+}
+
+func parseCmdlArgs() (string, int, error) {
+	algorithmName := flag.String("algorithm", "", "TrivialSort | MergeSort")
+	arraySize := flag.Int("size", 0, "Size of the array to be sorted")
+	flag.Parse()
+
+	if *algorithmName == "" {
+		return *algorithmName, *arraySize, errors.New("flag is mandatory: -algorithm")
+	}
+	if *arraySize == 0 {
+		return *algorithmName, *arraySize, errors.New("falg is mandatory: -size")
+	}
+
+	return *algorithmName, *arraySize, nil
+}
